f555843234eec31b23e592302b3aeb41933dd2cf
megasys <megasys@intro.hu>
2021-01-20 01:21:15 +0100

    Setup.AvaloniaRx: minimal sample project with ReactiveUI

---
+++ Setup.Avalonia/Setup.AvaloniaRx/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using Setup.AvaloniaRx.ViewModels;
+
+namespace Setup.AvaloniaRx
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+            
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ Setup.Avalonia/Setup.AvaloniaRx/ViewModels/ShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Avalonia.Controls.Chrome;
+using Caliburn.Micro;
+using ReactiveUI;
+
+namespace Setup.AvaloniaRx.ViewModels
+{
+    public class ShellViewModel : Conductor<Screen>.Collection.OneActive
+    {
+        private ObservableAsPropertyHelper<string> _displayNameRx;
+
+        public ShellViewModel()
+        {
+            _displayNameRx = this.WhenAnyValue(x => x.DisplayName)
+                .Select(s => s + " With ReactiveUI!")
+                .ToProperty(this, x => x.DisplayNameRx);
+        }
+
+        protected override async Task OnInitializeAsync(CancellationToken cancellationToken)
+        {
+            await base.OnInitializeAsync(cancellationToken);
+
+            DisplayName = "Welcome to Caliburn.Micro.Avalonia!";
+        }
+
+        public string DisplayNameRx => _displayNameRx.Value;
+    }
+}
